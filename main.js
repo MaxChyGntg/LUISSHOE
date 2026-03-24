d038eca231d7795541625d55888927d3048bb14c
MaxyChyGntg <sportznoah@gmail.com>
2026-03-25 02:30:21 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const navMenu = document.getElementById('nav-list')
+const navtoggle = document.getElementById('nav-toggle')
+const navclose = document.getElementById('nav-close')
+
+if (navtoggle) {
+  navtoggle.addEventListener("click", () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+if (navclose) {
+  navclose.addEventListener("click", () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+const buttons = document.querySelectorAll("#showmore");
+
+
+buttons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const card = btn.closest(".card");
+    card.classList.toggle("active");
+  });
+});
+
+const toggle = document.getElementById("dark-toggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggle.innerHTML = '<i class="bi bi-sun"></i>';
+  } else {
+    toggle.innerHTML = '<i class="bi bi-moon"></i>';
+  }
+});
+
